bf305036e1a5ccaeadd62c150dcb97404cb6217a
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-08-10 14:00:33 +0600

    tailwind css, font setup done

---
+++ src/Text.jsx
@@ -0,0 +1,11 @@
+
+const Text = () => {
+    return (
+        <div>
+            <h1 className="font-playfair-display-y text-5xl">Start Now</h1>
+            <h1 className="font-sans-s">Mosharof</h1>
+        </div>
+    );
+};
+
+export default Text;
